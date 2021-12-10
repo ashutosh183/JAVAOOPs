0db8bb2336f07d1f8573eb987542cd986804d995
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-10 12:24:11 +0530

    Added getRemainder()

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Calculator{
       private double firstNumber;
       private double secondNumber;
@@ -22,16 +24,28 @@ class Calculator{
       public double getMultiplication(){
             return this.firstNumber * this.secondNumber;
       }
+      public double getSubtraction(){
+            return Math.abs(this.firstNumber - this.secondNumber);
+      }
+      public double getRemainder(){
+            return this.firstNumber % this.secondNumber;
+      }
 }
 
 public class SimpleCalculator{
       public static void main(String[] args){
             Calculator val = new Calculator();
-            val.setFirstNumber(10);
-            val.setSecondNumber(20);
+            double num1, num2;
+            Scanner sc = new Scanner(System.in);
+            num1 = sc.nextDouble();
+            num2 = sc.nextDouble();
+            val.setFirstNumber(num1);
+            val.setSecondNumber(num2);
 
             System.out.println(val.getAdditionResult());
+            System.out.println(val.getSubtraction());
             System.out.println(val.getMultiplication());
             System.out.println(val.getDivisionResult());
+            System.out.println(val.getRemainder());
       }
 }
