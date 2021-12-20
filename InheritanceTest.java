e43aa18b17e03d4d33eae67e91366ae07a8eeeb6
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-20 12:17:20 +0530

    Learnt Override method

--- InheritanceTest.java
+++ InheritanceTest.java
@@ -25,6 +25,9 @@ class Animal{
       public void eat(){
             System.out.println("Animal eat is called");
       }
+      public void move(int speed){
+            System.out.println("Animal is moving at a speed of "+speed);
+      }
 }
 
 class Dog extends Animal{
@@ -46,6 +49,23 @@ class Dog extends Animal{
             System.out.println("Dog class eat is called");
             super.eat();
       }
+      public void walk(){
+            System.out.println("Dog walks");
+            move(5); /// can also call using super.move(value);
+      }
+      public void run(){
+            System.out.println("Dog runs");
+            move(10);
+      }
+      private void moveWithLegs(){
+            System.out.println("Legs");
+      }
+      @Override
+      public void move(int speed){
+            System.out.print("Dogs move with their ");
+            moveWithLegs();
+            super.move(5);
+      }
 }
 
 class InheritanceTest{
@@ -56,5 +76,7 @@ class InheritanceTest{
             System.out.println(dog1.getAge());
             dog1.eat();
             System.out.println(dog1.getName());
+            dog1.walk();
+            dog1.run();
       }
 }
